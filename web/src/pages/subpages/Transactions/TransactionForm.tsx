1867215243054b709342e03e43fe4118f6c10842
Long <somethings.ttt@gmail.com>
2025-04-09 22:56:37 +0700

    add saving to source and destination account list on TransactionForm

--- web/src/pages/subpages/Transactions/TransactionForm.tsx
+++ web/src/pages/subpages/Transactions/TransactionForm.tsx
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { Form, InputNumber, Button, DatePicker, Radio, Select, Space, Input } from "antd";
-import { Account } from "@/models/Account"; // Import Account model
-import { Transaction } from "@/models/Transaction"; // Import Transaction model
-import { Category } from "@/models/Category"; // Import Category model
-import { getStoredAccounts } from "@/services/accountService"; // Account service
-import { getStoredCategories } from "@/services/categoryService"; // Category service
-import dayjs from "dayjs"; // Using dayjs
+import {
+    Form,
+    InputNumber,
+    Button,
+    DatePicker,
+    Radio,
+    Select,
+    Space,
+    Input
+} from "antd";
+import { Account } from "@/models/Account";
+import { Transaction } from "@/models/Transaction";
+import { Category } from "@/models/Category";
+import { getStoredAccounts } from "@/services/accountService";
+import { getStoredSavings } from "@/services/savingService"; // New import
+import { getStoredCategories } from "@/services/categoryService";
+import dayjs from "dayjs";
 import { useRefresh } from "../../../context/RefreshProvider";
 
 interface TransactionFormProps {
@@ -18,16 +28,14 @@ const TransactionForm: React.FC<TransactionFormProps> = ({ transaction, onSubmit
     const [form] = Form.useForm();
     const [transactionType, setTransactionType] = useState<string>('income');
     const [accounts, setAccounts] = useState<Account[]>([]);
+    const [savings, setSavings] = useState<Account[]>([]);
     const [categories, setCategories] = useState<Category[]>([]);
     const { triggerRefresh } = useRefresh();
 
+    const combinedAccounts = [...accounts.map(a => ({ ...a, type: "account" })), ...savings.map(s => ({ ...s, type: "saving" }))];
+
     useEffect(() => {
-        const {
-            sourceAccount,
-            destinationAccount,
-            category,
-            ...rest
-        } = transaction;
+        const { sourceAccount, destinationAccount, category, ...rest } = transaction;
 
         form.setFieldsValue({
             ...rest,
@@ -37,46 +45,53 @@ const TransactionForm: React.FC<TransactionFormProps> = ({ transaction, onSubmit
         });
     }, [transaction]);
 
-
-    // Fetch accounts and categories
     useEffect(() => {
-        const fetchAccountsAndCategories = async () => {
-            const accountsData = await getStoredAccounts();
-            setAccounts(accountsData);
-            const categoriesData = await getStoredCategories();
-            setCategories(categoriesData);
+        const fetchAll = async () => {
+            const [accs, savs, cats] = await Promise.all([
+                getStoredAccounts(),
+                getStoredSavings(),
+                getStoredCategories()
+            ]);
+            setAccounts(accs);
+            setSavings(savs);
+            setCategories(cats);
         };
 
-        fetchAccountsAndCategories();
+        fetchAll();
     }, []);
 
-    // Handle transaction type change
     const handleTransactionTypeChange = (e: any) => {
         setTransactionType(e.target.value);
     };
 
     const handleFinish = (values: any) => {
-        // Ensure dateTime is a valid dayjs or Date object
         const updatedTransaction = {
             ...values,
             _id: transaction._id,
-            dateTime: values.dateTime instanceof dayjs ? values.dateTime.toISOString() : values.dateTime, // Convert to Date if it's a dayjs object
-            sourceAccount: values.sourceAccount || '000000000000000000000000',  // Fallback UUID
-            destinationAccount: values.destinationAccount || '000000000000000000000000',  // Fallback UUID
+            dateTime: values.dateTime instanceof dayjs ? values.dateTime.toISOString() : values.dateTime,
+            sourceAccount: values.sourceAccount || '000000000000000000000000',
+            destinationAccount: values.destinationAccount || '000000000000000000000000',
             category: values.category || '000000000000000000000000',
             creator: localStorage.getItem("username"),
             isDeleted: false,
-            note: values.note || '', // Ensure note is passed, default to empty string if not specified
+            note: values.note || '',
         };
         onSubmit(updatedTransaction);
     };
 
+    const renderAccountOptions = () =>
+        combinedAccounts.map(acc => (
+            <Select.Option key={acc._id} value={acc._id}>
+                [{acc.type === "account" ? "A" : "S"}] {acc.name}
+            </Select.Option>
+        ));
+
     return (
         <Form
             form={form}
-            layout="horizontal" // Switch from "vertical" to "horizontal"
-            labelCol={{ span: 8 }} // Width of the label column
-            wrapperCol={{ span: 16 }} // Width of the input column
+            layout="horizontal"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
             labelAlign="left"
             requiredMark={false}
             onFinish={handleFinish}
@@ -90,12 +105,13 @@ const TransactionForm: React.FC<TransactionFormProps> = ({ transaction, onSubmit
                     </Radio.Group>
                 </div>
             </Form.Item>
+
             <Form.Item
                 name="dateTime"
                 label="Date"
                 rules={[{ required: true }]}
                 getValueProps={(value) => ({
-                    value: value ? dayjs(value) : "", // Convert value to dayjs object
+                    value: value ? dayjs(value) : "",
                 })}
             >
                 <DatePicker showTime style={{ width: "100%" }} />
@@ -105,17 +121,10 @@ const TransactionForm: React.FC<TransactionFormProps> = ({ transaction, onSubmit
                 <InputNumber style={{ width: "100%" }} />
             </Form.Item>
 
-            {/* Conditionally render based on transaction type */}
             {transactionType === 'income' && (
                 <>
                     <Form.Item name="destinationAccount" label="Destination Account" rules={[{ required: true }]}>
-                        <Select>
-                            {accounts.map(account => (
-                                <Select.Option key={account._id} value={account._id}>
-                                    {account.name}
-                                </Select.Option>
-                            ))}
-                        </Select>
+                        <Select>{renderAccountOptions()}</Select>
                     </Form.Item>
 
                     <Form.Item name="category" label="Category" rules={[{ required: true }]}>
@@ -133,13 +142,7 @@ const TransactionForm: React.FC<TransactionFormProps> = ({ transaction, onSubmit
             {transactionType === 'expense' && (
                 <>
                     <Form.Item name="sourceAccount" label="Source Account" rules={[{ required: true }]}>
-                        <Select>
-                            {accounts.map(account => (
-                                <Select.Option key={account._id} value={account._id}>
-                                    {account.name}
-                                </Select.Option>
-                            ))}
-                        </Select>
+                        <Select>{renderAccountOptions()}</Select>
                     </Form.Item>
 
                     <Form.Item name="category" label="Category" rules={[{ required: true }]}>
@@ -157,28 +160,15 @@ const TransactionForm: React.FC<TransactionFormProps> = ({ transaction, onSubmit
             {transactionType === 'transfer' && (
                 <>
                     <Form.Item name="sourceAccount" label="Source Account" rules={[{ required: true }]}>
-                        <Select>
-                            {accounts.map(account => (
-                                <Select.Option key={account._id} value={account._id}>
-                                    {account.name}
-                                </Select.Option>
-                            ))}
-                        </Select>
+                        <Select>{renderAccountOptions()}</Select>
                     </Form.Item>
 
                     <Form.Item name="destinationAccount" label="Destination Account" rules={[{ required: true }]}>
-                        <Select>
-                            {accounts.map(account => (
-                                <Select.Option key={account._id} value={account._id}>
-                                    {account.name}
-                                </Select.Option>
-                            ))}
-                        </Select>
+                        <Select>{renderAccountOptions()}</Select>
                     </Form.Item>
                 </>
             )}
 
-            {/* Add Note Field */}
             <Form.Item name="note" label="Note">
                 <Input.TextArea placeholder="Enter a note" />
             </Form.Item>
