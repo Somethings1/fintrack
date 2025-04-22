2bdc60d0a3b1b57fd8f81e6e16fc14f12d672dff
Long <somethings.ttt@gmail.com>
2025-04-22 16:49:05 +0700

    color json

---
+++ web/src/theme/color.ts
@@ -0,0 +1,50 @@
+export const colors = {
+  // Primary · Purple
+  primary: {
+    900: "#5347A1",
+    800: "#6657C6",
+    700: "#7664E4",
+    600: "#8470FF",
+    500: "#A498FF",
+    400: "#BFB7FF",
+    300: "#D6D2FF",
+    200: "#ECEAFF",
+    100: "#F5F4FF",
+  },
+
+  // Neutral · Gray
+  neutral: {
+    900: "#111113",
+    800: "#222226",
+    700: "#343438",
+    600: "#45454B",
+    500: "#56565E",
+    400: "#82828C",
+    300: "#A1A1A9",
+    200: "#D0D0D4",
+    100: "#EFEFF1",
+  },
+
+  // Black & White
+  black: "#000000",
+  white: "#FFFFFF",
+
+  // Success · Green
+  success: {
+    800: "#297B32",
+    200: "#E5FAE6",
+  },
+
+  // Warning · Orange
+  warning: {
+    500: "#F9970C",
+    200: "#FEF4E1",
+  },
+
+  // Danger · Red
+  danger: {
+    500: "#E83838",
+    200: "#FFEBEB",
+  },
+} as const;
+
