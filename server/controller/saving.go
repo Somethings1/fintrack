190b0cd06e4f359ee73c2180be216cbf4516ef02
Long <somethings.ttt@gmail.com>
2025-05-23 11:47:38 +0700

    split saving controller and service

--- server/controller/saving.go
+++ server/controller/saving.go
@@ -1,20 +1,19 @@
 package controller
 
 import (
-    "io"
-    "fmt"
-    "time"
-    "context"
-    "net/http"
-    "encoding/json"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
 
-    "fintrack/server/util"
-    "fintrack/server/model"
+	"fintrack/server/model"
+	"fintrack/server/service"
+	"fintrack/server/util"
 
-    "github.com/gin-gonic/gin"
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/mongo/options"
-    "go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 //////////////////
@@ -43,36 +42,6 @@ func AddSaving(c *gin.Context) {
     })
 }
 
-func GetSavings(c *gin.Context) {
-    filter := bson.M{"owner": c.GetString("username")}
-
-    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-    defer cancel()
-
-    cursor, err := util.SavingCollection.Find(ctx, filter)
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Error fetching savings"})
-        return
-    }
-    defer cursor.Close(ctx)
-
-    c.Header("Content-Type", "application/json")
-    c.Status(http.StatusOK)
-
-    c.Stream(func(w io.Writer) bool {
-        if cursor.Next(ctx) {
-            var saving model.Saving
-            if err := cursor.Decode(&saving); err != nil {
-                fmt.Println("Error decoding saving:", err)
-                return false
-            }
-            json.NewEncoder(w).Encode(saving)
-            return true
-        }
-        return false
-    })
-}
-
 func GetSavingsSince(c *gin.Context) {
     sinceStr := c.Param("time")
     sinceTime, err := time.Parse(time.RFC3339, sinceStr)
@@ -81,21 +50,10 @@ func GetSavingsSince(c *gin.Context) {
         return
     }
 
-    filter := bson.M{
-        "last_update": bson.M{
-            "$gt": sinceTime,
-        },
-        "owner": c.GetString("username"),
-    }
-
     ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
     defer cancel()
 
-    opts := options.Find().SetSort(bson.D{
-        {Key: "last_update", Value: -1},
-    })
-
-    cursor, err := util.SavingCollection.Find(ctx, filter, opts)
+    cursor, err := service.FetchSavingsSince(ctx, c.GetString("username"), sinceTime)
     if err != nil {
         c.JSON(http.StatusInternalServerError, gin.H{"error": "Error fetching savings"})
         return
@@ -128,15 +86,10 @@ func UpdateSaving(c *gin.Context) {
         return
     }
 
-    // Set the last_update field to the current time
-    saving.LastUpdate = time.Now()
-
-    filter := bson.M{"_id": id}
-
     ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
     defer cancel()
 
-    _, err = util.SavingCollection.UpdateOne(ctx, filter, bson.M{"$set": saving})
+    err = service.UpdateSaving(ctx, id, saving)
     if err != nil {
         c.JSON(http.StatusInternalServerError, gin.H{"error": "Error updating saving"})
         return
@@ -145,7 +98,6 @@ func UpdateSaving(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{"message": "Saving updated successfully"})
 }
 
-// DeleteSaving performs a soft delete by marking the saving and its related transactions as deleted
 func DeleteSaving(c *gin.Context) {
 	id, err := primitive.ObjectIDFromHex(c.Param("id"))
 	if err != nil {
@@ -156,35 +108,12 @@ func DeleteSaving(c *gin.Context) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	// Step 1: Soft delete the saving
-	savingUpdate := bson.M{
-		"$set": bson.M{
-			"is_deleted":  true,
-			"last_update": time.Now(),
-		},
-	}
-	_, err = util.SavingCollection.UpdateOne(ctx, bson.M{"_id": id}, savingUpdate)
+    err = service.DeleteSaving(ctx, id)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error soft deleting saving"})
-		return
-	}
-
-	// Step 2: Soft delete related transactions (where saving is source or destination)
-	transactionUpdate := bson.M{
-		"$set": bson.M{
-			"is_deleted":  true,
-			"last_update": time.Now(),
-		},
-	}
-	filter := bson.M{
-		"$or": []bson.M{
-			{"source_account": id},
-            {"destination_account": id},
-		},
-	}
-	_, err = util.TransactionCollection.UpdateMany(ctx, filter, transactionUpdate)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error soft deleting related transactions"})
+		c.JSON(http.StatusInternalServerError, gin.H{
+            "error": "Error soft deleting related transactions",
+            "detail": err.Error(),
+        })
 		return
 	}
 
--- server/service/saving.go
+++ server/service/saving.go
@@ -1,16 +1,18 @@
 package service
 
 import (
-    "context"
-    "errors"
-    "time"
+	"context"
+	"errors"
+	"fmt"
+	"time"
 
-    "fintrack/server/model"
-    "fintrack/server/util"
+	"fintrack/server/model"
+	"fintrack/server/util"
 
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/bson/primitive"
-    "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func GetSavingByID(id string) (model.Saving, error) {
@@ -33,4 +35,60 @@ func GetSavingByID(id string) (model.Saving, error) {
     }
 
     return saving, nil
+}
+
+func FetchSavingsSince (ctx context.Context, username string, since time.Time) (*mongo.Cursor, error) {
+    filter := bson.M{
+        "last_update": bson.M{
+            "$gt": since,
+        },
+        "owner": username,
+    }
+
+    opts := options.Find().SetSort(bson.D{
+        {Key: "last_update", Value: -1},
+    })
+
+    return util.SavingCollection.Find(ctx, filter, opts)
+}
+
+func UpdateSaving (ctx context.Context, id primitive.ObjectID, saving model.Saving) error {
+    saving.LastUpdate = time.Now()
+    filter := bson.M{"_id": id}
+    updateSaving := bson.M{"$set": saving}
+
+    _, err := util.SavingCollection.UpdateOne(ctx, filter, updateSaving)
+    return err
+}
+
+func DeleteSaving (ctx context.Context, id primitive.ObjectID) error {
+	savingUpdate := bson.M{
+		"$set": bson.M{
+			"is_deleted":  true,
+			"last_update": time.Now(),
+		},
+	}
+    _, err := util.SavingCollection.UpdateOne(ctx, bson.M{"_id": id}, savingUpdate)
+	if err != nil {
+        return fmt.Errorf("Error deleting saving: %w", err)
+	}
+
+	transactionUpdate := bson.M{
+		"$set": bson.M{
+			"is_deleted":  true,
+			"last_update": time.Now(),
+		},
+	}
+	filter := bson.M{
+		"$or": []bson.M{
+			{"source_account": id},
+            {"destination_account": id},
+		},
+	}
+	_, err = util.TransactionCollection.UpdateMany(ctx, filter, transactionUpdate)
+	if err != nil {
+        return fmt.Errorf("Error deleting related transactions: %w", err)
+	}
+
+    return nil
 }
